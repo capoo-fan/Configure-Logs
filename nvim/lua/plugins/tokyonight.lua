74d80889f302f4a204932004c7e842effb398211
qianfu <1733034367@qq.com>
2025-02-28 15:28:08 +0800

    erase

--- nvim/lua/plugins/tokyonight.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "folke/tokyonight.nvim",
-    opts = {
-        transparent = true,
-        styles = {sidebars = "transparent", floats = "transparent"},
-        terminal_colors = true -- Configure the colors used when opening a `:terminal` in Neovim
-    }
-}
